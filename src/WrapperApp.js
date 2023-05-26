372485ea2c35fb76c34c65d29db9f2eaaaf5b286
clivehibberd <clive33@mac.com>
2023-05-26 10:47:03 +0300

    Added Simple Nav

--- src/WrapperApp.js
+++ src/WrapperApp.js
@@ -4,11 +4,13 @@ import "@aws-amplify/ui-react/styles.css";
 import SlugApp from "./SlugApp.js";
 import  CharacteristicsSelector  from "./components/CharacteristicsSelector";
 import { ApplicationProvider } from "./components/applicationcontext";
+import MainNav from "./components/MainNav.js";
 
 export default function WrapperApp() {
   return (
     <ApplicationProvider>
       <div className="App">
+    <MainNav/>
         <CharacteristicsSelector />
         <SlugApp/>
       </div>
--- src/index.js
+++ src/index.js
@@ -15,10 +15,6 @@ Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <View>
-    <Heading level={1}>SLUGHORN TEST</Heading>
-    </View>
-     
     <WrapperApp/>
   </React.StrictMode>
 );

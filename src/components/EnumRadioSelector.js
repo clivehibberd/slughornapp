32cd2544e3be2d069240b5752995f25fe5e77f9a
clivehibberd <clive33@mac.com>
2023-06-12 12:02:47 +0100

    Removed Clear logic - dont like the UX

--- src/components/EnumRadioSelector.js
+++ src/components/EnumRadioSelector.js
@@ -8,12 +8,11 @@ import RadioGroup from "@mui/joy/RadioGroup";
 import List from "@mui/joy/List";
 import ListItem from "@mui/joy/ListItem";
 import ListItemDecorator from "@mui/joy/ListItemDecorator";
-import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
-import Radio, { radioClasses } from "@mui/joy/Radio";
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
+import Radio, { radioClasses } from '@mui/joy/Radio';
 
 import { listEnums } from "../graphql/slugqueries";
 import imagemapjson from "./static/imagemap.json";
-import { useContext } from "react";
 
 /**
  * The Generic RadioEnum Component
@@ -24,25 +23,28 @@ export default function EnumRadioGroup({ enumType, enumMap }) {
   const [enumList, setEnumList] = useState([]);
   const [radiovalue, setRadioValue] = useState([]);
   const savedEnumReducer = (state, action) => {
+    
     const { enumType, enumValue, type } = action;
-    const element = document.getElementById(enumType);
-    element.checked = !element.checked;
-    //console.log("In reducer Action type:",type, "checked is now", element.checked);
-    //console.log("Reducer state is ", state);
+    console.log("Reducer state is ", state);
     if (type === "add") {
-      //console.log("Adding to enumMap", enumValue);
+      console.log("Adding to enumMap", enumValue);
       enumMap.set(enumType, enumValue);
-    } else if (type === "remove") {
-      //console.log("Unchecking the current RadioGroup", enumType);
+    } else if (type === "clear") {
+      console.log("Clearing the current RadioGroup", enumType);
       enumMap.delete(enumType);
+      const element = document.getElementById(enumType);
+      element.checked = false;
     }
     return enumMap;
   };
-
   const [selectedEnum, setSelectedEnum] = useReducer(
     savedEnumReducer,
     new Map()
   );
+  const addNewValue = (enumType, enumValue) => {
+    console.log("Adding ", enumType, enumValue);
+    setSelectedEnum({ enumType, enumValue, type: "add" });
+  };
 
   useEffect(() => {
     getEnums();
@@ -51,29 +53,23 @@ export default function EnumRadioGroup({ enumType, enumMap }) {
   const updateSelection = (event) => {
     var type = null;
     var enumValue = "";
-    const targetChecked = event.target.checked;
-    if (targetChecked) {
+    if (radiovalue.length === 0) {
+      // If currently unset
       enumValue = event.target.value;
       type = "add";
-    } else if (!targetChecked) {
-      type = "remove";
+    } else if (radiovalue === event.target.value) {
+      // If its the same, clear the selection
+      type = "clear";
       enumValue = "";
-    } 
-    /*
-    console.log(
-      "Updating selection to ",
-      enumType,
-      " - ",
-      enumValue,
-      " - ",
-      type
-    );
-    */
+      event.target.checked = false;
+    } else {
+      type = "add";
+      enumValue = event.target.value;
+    }
+    console.log("Updating selection to ", enumValue);
     setRadioValue(enumValue);
     setSelectedEnum({ enumType, enumValue, type: type });
   };
-
-  
 
   /**
    * Get the enum list from the GraphQl API and set it in the state
@@ -116,95 +112,90 @@ export default function EnumRadioGroup({ enumType, enumMap }) {
   }
 
   return (
-    <div>
-      <RadioGroup
-        aria-label="Your plan"
-        name="people"
+    <RadioGroup aria-label="Your plan" name="people"
+    sx={{
+      flexDirection: 'row',
+      gap: 2,
+      [`& .${radioClasses.checked}`]: {
+        [`& .${radioClasses.action}`]: {
+          inset: -1,
+          border: '3px solid',
+          borderColor: 'primary.500',
+        },
+      },
+      [`& .${radioClasses.radio}`]: {
+        display: 'contents',
+        '& > svg': {
+          zIndex: 2,
+          position: 'absolute',
+          top: '-8px',
+          right: '-8px',
+          bgcolor: 'background.body',
+          borderRadius: '50%',
+        },
+      },
+    }}>
+      <List
+        orientation="horizontal"
         sx={{
-          flexDirection: "row",
-          gap: 2,
-          [`& .${radioClasses.checked}`]: {
-            [`& .${radioClasses.action}`]: {
-              inset: -1,
-              border: "3px solid",
-              borderColor: "primary.500",
-            },
-          },
-          [`& .${radioClasses.radio}`]: {
-            display: "contents",
-            "& > svg": {
-              zIndex: 2,
-              position: "absolute",
-              top: "-8px",
-              right: "-8px",
-              bgcolor: "background.body",
-              borderRadius: "50%",
-            },
-          },
+          minWidth: 0,
+          "--List-gap": "0.5rem",
+          "--ListItem-paddingY": "1rem",
+          "--ListItem-paddingX": "0.25rem",
+          "--ListItem-radius": "8px",
+          "--ListItemDecorator-size": "32px",
         }}
       >
-        <List
-          orientation="horizontal"
-          sx={{
-            minWidth: 0,
-            "--List-gap": "0.5rem",
-            "--ListItem-paddingY": "1rem",
-            "--ListItem-paddingX": "0.25rem",
-            "--ListItem-radius": "8px",
-            "--ListItemDecorator-size": "32px",
-          }}
-        >
-          {enumList.map((value, index) => (
-            <ListItem
-              variant="outlined"
-              key={value.name}
-              sx={{
-                width: 50,
-                height: 50,
-                boxShadow: "sm",
-                backgroundColor: "blue",
-                bgcolor: "background.body",
-              }}
+        {enumList.map((value, index) => (
+          
+          <ListItem
+            variant="outlined"
+            key={value.name}
+            sx={{
+              width: 50,
+              height:50,
+              boxShadow: "sm",
+              backgroundColor: "blue",
+              bgcolor: "background.body",
+            }}
+            value={value.name}
+          >
+            
+            <ListItemDecorator>
+              <img
+                height="40px"
+                alt={value.name}
+                src={getImage(value.name)}
+                sx={{ width: 30, height: 30 }}
+              />
+            </ListItemDecorator>
+            
+            <Radio
+              id={enumType}
+              checkedIcon={<CheckCircleRoundedIcon />}
+              overlay
               value={value.name}
-            >
-              <ListItemDecorator>
-                <img
-                  height="40px"
-                  alt={value.name}
-                  src={getImage(value.name)}
-                  sx={{ width: 30, height: 30 }}
-                />
-              </ListItemDecorator>
-
-              <Radio
-                id={enumType}
-                checkedIcon={<CheckCircleRoundedIcon />}
-                overlay
-                value={value.name}
-                onClick={updateSelection}
-                
-                checked={
-                  radiovalue === value.name && enumMap.has(enumType)
-                }
-                sx={{ flexGrow: 1, flexDirection: "row-reverse" }}
-                slotProps={{
-                  action: ({ checked }) => ({
-                    sx: (theme) => ({
-                      ...(checked && {
-                        inset: 2,
-                        border: "5px solid",
-                        borderColor: "red",
-                      }),
+              onClick={updateSelection}
+              checked={radiovalue === value.name}
+              sx={{ flexGrow: 1, flexDirection: "row-reverse" }}
+              slotProps={{
+                action: ({ checked }) => ({
+                  sx: (theme) => ({
+                    ...(checked && {
+                      inset: 2,
+                      border: "5px solid",
+                      borderColor: "red",
                     }),
                   }),
-                }}
-              />
-              
-            </ListItem>
-          ))}
-        </List>
-      </RadioGroup>
-    </div>
+                }),
+              }}
+            />
+
+          </ListItem>
+          
+        ))}
+      </List>
+    </RadioGroup>
   );
 }
 
